c3ec4e51e19b0df235bba482a4f439886ea360e3
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-11-22 20:59:38 +0100

    Add page to view all questions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
-const Knex = require("knex");
-const bodyParser = require("body-parser");
-const knexConfig = require("../knexfile");
+const Knex = require('knex');
+const bodyParser = require('body-parser');
+const knexConfig = require('../knexfile');
 
 var knex = Knex(knexConfig);
 
-app.use(express.static("public"));
+app.use('/assets', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (_req, res) => {
+  res.render('index', { title: 'Skicka en fråga' });
+});
+
 app.post('/', async (req, res) => {
-  await knex.table("questions").insert({
-    question: req.body.fraga
+  await knex.table('questions').insert({
+    question: req.body.question
   });
 
-  res.redirect('/tack.html');
+  res.redirect('/thanks');
+});
+
+app.get("/thanks", (_req, res) => {
+  res.render("thanks", { title: "Tack" });
+});
+
+app.get('/questions', async (_req, res) => {
+  const questions = await knex.table('questions').select();
+  res.render("questions", { 
+    questions: questions.map(({ question }) => question),
+    title: 'Frågor',
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
